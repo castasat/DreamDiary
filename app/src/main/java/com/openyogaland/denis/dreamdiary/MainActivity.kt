201c5c120cd9f246d540110c95396a00100f255c
castasat <castasat@mail.ru>
2019-06-16 12:54:28 +0300

    - added MainActivity.loadFragment() - added MainActivity.clearFragmentBackStack() - added DayFragment.kt and day_fragment.xml

--- app/src/main/java/com/openyogaland/denis/dreamdiary/MainActivity.kt
+++ app/src/main/java/com/openyogaland/denis/dreamdiary/MainActivity.kt
@@ -1,13 +1,54 @@
 package com.openyogaland.denis.dreamdiary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.openyogaland.denis.dreamdiary.view.fragment.DayFragment
 
-class MainActivity : AppCompatActivity()
+public class
+MainActivity : AppCompatActivity()
 {
-    override fun onCreate(savedInstanceState: Bundle?)
+  private var dayFragment : DayFragment? = null
+  
+  override fun onCreate(savedInstanceState : Bundle?)
+  {
+    super
+    .onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    
+    dayFragment = DayFragment()
+    loadFragment(dayFragment)
+  }
+  
+  private fun loadFragment(fragmentToLoad : Fragment?)
+  {
+    val fragmentManager : FragmentManager = supportFragmentManager
+    clearFragmentBackStack(fragmentManager)
+    
+    val transaction : FragmentTransaction = fragmentManager.beginTransaction()
+    
+    if((fragmentToLoad != null) && (!fragmentToLoad.isAdded))
     {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+      when(fragmentToLoad)
+      {
+        is DayFragment ->
+          transaction.add(R.id.main_activity_content, fragmentToLoad)
+      }
     }
+  
+    transaction.commit()
+    transaction.addToBackStack(null)
+  }
+  
+  private fun clearFragmentBackStack(fragmentManager : FragmentManager)
+  {
+    val backStackEntryCount : Int = fragmentManager.backStackEntryCount
+    
+    for(count in backStackEntryCount downTo 1)
+    {
+      fragmentManager.popBackStack()
+    }
+  }
 }
---
+++ app/src/main/java/com/openyogaland/denis/dreamdiary/view/fragment/DayFragment.kt
@@ -0,0 +1,23 @@
+package com.openyogaland.denis.dreamdiary.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.openyogaland.denis.dreamdiary.R
+
+public class
+DayFragment : Fragment()
+{
+  override fun onCreateView(inflater : LayoutInflater,
+                            container : ViewGroup?,
+                            savedInstanceState : Bundle?) : View?
+  {
+    val view : View =
+      inflater.inflate(R.layout.day_fragment,
+                       container,
+                       false)
+    return view
+  }
+}
