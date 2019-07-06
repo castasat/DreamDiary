9fec04cb08d385bd07cb81457ad158ee1b89b1db
castasat <castasat@mail.ru>
2019-07-06 03:11:11 +0300

    - added image to collapsing toolbar layout

--- app/src/main/java/com/openyogaland/denis/dreamdiary/MainActivity.kt
+++ app/src/main/java/com/openyogaland/denis/dreamdiary/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.openyogaland.denis.dreamdiary
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import androidx.palette.graphics.Palette
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.openyogaland.denis.dreamdiary.R.id
-import kotlinx.android.synthetic.main.activity_main.bottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 public class
 MainActivity : AppCompatActivity()
 {
   private lateinit var navController : NavController
+  private lateinit var collapsingToolbarLayout : CollapsingToolbarLayout
+  private lateinit var toolbar : Toolbar
   
   override fun
   onCreate(savedInstanceState : Bundle?)
@@ -22,20 +30,64 @@ MainActivity : AppCompatActivity()
     setContentView(R.layout.activity_main)
     
     navController = findNavController(this, id.navigationHostFragment)
-    
     NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    
+    collapsingToolbarLayout =
+      findViewById(id.collapsingToolbarLayout)
+  
+    toolbar =
+      findViewById(id.toolbar)
+    
+    val appBarConfiguration =
+      AppBarConfiguration(navController.graph)
+  
+    collapsingToolbarLayout
+    .setupWithNavController(toolbar, 
+                            navController, 
+                            appBarConfiguration)
+    
+    collapsingToolbarLayoutDynamicColor()
   }
   
-  override fun onOptionsItemSelected(item : MenuItem) : Boolean
+  private fun
+  collapsingToolbarLayoutDynamicColor()
   {
-    when (item.itemId)
+   /* val bitmap = BitmapFactory
+    .decodeResource(resources, R.drawable.night_collapsing_toolbar_background)
+    
+    Palette
+    .from(bitmap)
+    .generate {palette ->
+      
+      palette
+      ?.let {
+        collapsingToolbarLayout
+        .setContentScrimColor(palette
+                              .getMutedColor(resources
+                                             .getColor(R.color
+                                                       .colorPrimary)))
+        collapsingToolbarLayout
+        .setStatusBarScrimColor(palette
+                                .getMutedColor(resources
+                                               .getColor(R.color
+                                                         .colorPrimaryDark)))
+      }
+    }*/
+  }
+  
+  override fun
+  onOptionsItemSelected(item : MenuItem) : Boolean
+  {
+    when(item.itemId)
     {
       android.R.id.home ->
       {
-        navController.popBackStack()
+        navController
+        .popBackStack()
         return true
       }
     }
-    return super.onOptionsItemSelected(item)
+    return super
+    .onOptionsItemSelected(item)
   }
 }
