0f4aa631fe1c0bf52b49fdd04a2f722a767ee69f
castasat <castasat@mail.ru>
2019-07-22 00:38:42 +0300

    - TODO set custom attributes of spinner items

--- app/src/main/java/com/openyogaland/denis/dreamdiary/view/fragment/DayFragment.kt
+++ app/src/main/java/com/openyogaland/denis/dreamdiary/view/fragment/DayFragment.kt
@@ -1,14 +1,20 @@
 package com.openyogaland.denis.dreamdiary.view.fragment
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import androidx.fragment.app.Fragment
 import com.openyogaland.denis.dreamdiary.R
+import kotlinx.android.synthetic.main.day_fragment.*
 
 @Suppress("NAME_SHADOWING")
 public class
@@ -24,6 +30,49 @@ DayFragment : Fragment()
       .inflate(R.layout.day_fragment,
                container,
                false)
+    
+    val arrayAdapter =
+      ArrayAdapter
+      .createFromResource(requireActivity(),
+                          R.array.practice_types,
+                          android.R.layout.simple_spinner_item)
+    arrayAdapter
+    .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+    
+    practiceSpinner
+    ?.onItemSelectedListener =
+      object : AdapterView.OnItemSelectedListener
+      {
+        override fun
+        onItemSelected(parent : AdapterView<*>?,
+                       itemSelected : View?,
+                       selectedItemPosition : Int,
+                       selectedId : Long)
+        {
+          parent?.let{parent: AdapterView<*> ->
+            parent
+            .getChildAt(0)
+            ?.let {view : View ->
+              if(view is TextView)
+              {
+                view.setTextColor(resources
+                              .getColor(R.color.grayed_text_gray))
+                view.textSize = 12.0f
+                view.setPadding(0,0,0,0)
+              }
+            }
+          }
+        }
+        
+        override fun
+        onNothingSelected(parent : AdapterView<*>?)
+        {
+          // TODO
+        }
+      }
+    
+    practiceSpinner
+    ?.adapter = arrayAdapter
     
     /* TODO
     val stressLevelSeekBar =
