3fcb153f92a2b08dfd29a1200651b4c7ab8c9884
castasat <castasat@mail.ru>
2019-07-30 01:45:40 +0300

    - added input mask

--- app/src/main/java/com/openyogaland/denis/dreamdiary/view/fragment/DreamFragment.kt
+++ app/src/main/java/com/openyogaland/denis/dreamdiary/view/fragment/DreamFragment.kt
@@ -4,38 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.openyogaland.denis.dreamdiary.R
-import com.openyogaland.denis.dreamdiary.model.Dream
+import com.redmadrobot.inputmask.MaskedTextChangedListener
+import kotlinx.android.synthetic.main.dream_fragment.*
 
 public class
 DreamFragment : Fragment()
 {
-  companion object
-  {
-    private const val DREAM = "dream"
-    
-    fun bundleArgs(dream : Dream) : Bundle
-    {
-      return Bundle()
-      .apply{
-        this.putParcelable(DREAM, dream)
-      }
-    }
-  }
-  
-  override fun onCreateView(inflater : LayoutInflater,
-                            container : ViewGroup?,
-                            savedInstanceState : Bundle?) : View?
+  override fun
+  onCreateView(inflater : LayoutInflater,
+               container : ViewGroup?,
+               savedInstanceState : Bundle?) : View?
   {
     val view : View =
-      inflater.inflate(R.layout.dream_fragment, container, false)
+      inflater
+      .inflate(R.layout.dream_fragment, container, false)
     
     return view
   }
   
-  override fun onActivityCreated(savedInstanceState : Bundle?)
+  private fun
+  setInputMaskToEditText(format : String, editText : EditText)
   {
-    super.onActivityCreated(savedInstanceState)
+    val maskedTextChangedListener = MaskedTextChangedListener(format, editText)
+    editText
+    .addTextChangedListener(maskedTextChangedListener)
+    editText
+    .onFocusChangeListener = maskedTextChangedListener
+  }
+  
+  override fun
+  onActivityCreated(savedInstanceState : Bundle?)
+  {
+    super
+    .onActivityCreated(savedInstanceState)
+    
+    setInputMaskToEditText("[00]:[00] - [00]:[00]", dreamDurationTimeEditText)
   }
 }
